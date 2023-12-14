b1fe66c8c9ca8a96cfd0604e2c02c12f07b2963d
kraiglarson2 <kraigalarson2@gmail.com>
2023-12-14 14:29:35 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Krajg Larson on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
